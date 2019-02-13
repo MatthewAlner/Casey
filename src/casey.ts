4088de2147e610109ac50595041f2f3b1d90403c
Matthew Alner <matthew.alner@senseye.io>
2019-02-13 14:47:40 +0000

    wrapping in class

--- src/casey.ts
+++ src/casey.ts
@@ -4,33 +4,61 @@ import * as voca from 'voca';
 
 import { StringFunction } from './string-function';
 
-const desc = `
-choose a function to run
-    cc => toCamelCase
-    kc => to-kebab-case
-`;
+class Casey {
 
-const argv = yargs
-    .option('function', {
-    alias: 'f',
-    describe: desc,
-    type: 'string',
-    demand: true,
-    choices: Object.keys(StringFunction).map((k) => StringFunction[k])
-}).option('input', {
-    alias: 'i',
-    describe: 'provide string to transform',
-    type: 'string',
-    demand: true
-}).argv;
-
-switch(argv.function) {
-    case StringFunction.CAMEL_CASE: {
-        console.log(voca.camelCase(argv.input));
-        break;
+    private argv: { [x: string]: unknown; function: string; input: string; _: string[]; $0: string; };
+    run () {
+        this.parseArguments();
+        const output = this.convertString(this.argv.input, this.argv.function);
+        console.log(output);
     }
-    case StringFunction.KEBAB_CASE: {
-        console.log(voca.kebabCase(argv.input));
-        break;
+
+    parseArguments() {
+        this.argv = yargs
+            .option('function', {
+                alias: 'f',
+                describe: this.generateDescription(),
+                type: 'string',
+                demand: true,
+                choices: Object.keys(StringFunction).map((k) => StringFunction[k])
+            }).option('input', {
+                alias: 'i',
+                describe: 'provide string to transform',
+                type: 'string',
+                demand: true
+            }).argv;
+    }
+
+    convertString(input: string, func: string): string {
+        let output: string;
+
+        switch(func) {
+            case StringFunction.CAMEL_CASE: {
+                output = voca.camelCase(input);
+                break;
+            }
+            case StringFunction.KEBAB_CASE: {
+                output = voca.kebabCase(input);
+                break;
+            }
+            default: {
+                throw new Error(`Can't find function ${this.argv.function}`);
+            }
+        }
+        return output;
+    }
+
+    generateDescription(): string {
+        let desc: string[] = [];
+        desc.push('choose a function to run');
+        Object.entries(StringFunction).forEach(func => {
+            const [key, value] = func;
+            desc.push(`${value} => ${this.convertString(key, value)}`);
+        });
+        desc.push('');
+        return desc.join('\n')
     }
 }
+
+const casey = new Casey();
+casey.run();
