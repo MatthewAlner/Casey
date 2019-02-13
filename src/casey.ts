7331715d58039d6891eaa82c78aeaabf838a9119
Matthew Alner <matthew.alner@senseye.io>
2019-02-13 16:21:59 +0000

    remove enum

--- src/casey.ts
+++ src/casey.ts
@@ -2,14 +2,19 @@
 import * as yargs from 'yargs'
 import * as voca from 'voca';
 
-import { StringFunction } from './string-function';
+import { getAvailableFunctions, StringFunction } from './string-function';
 
 class Casey {
 
     private argv: { [x: string]: unknown; function: string; input: string; _: string[]; $0: string; };
+    private availableFunc: StringFunction[];
+    private funcToRun: StringFunction;
+    private input: string;
+
     run () {
+        this.availableFunc = getAvailableFunctions();
         this.parseArguments();
-        const output = this.convertString(this.argv.input, this.argv.function);
+        const output = this.convertString(this.input, this.funcToRun);
         console.log(output);
     }
 
@@ -20,30 +25,24 @@ class Casey {
                 describe: this.generateDescription(),
                 type: 'string',
                 demand: true,
-                choices: Object.keys(StringFunction).map((k) => StringFunction[k])
+                choices: this.availableFunc.map(func => func.alias)
             }).option('input', {
                 alias: 'i',
                 describe: 'provide string to transform',
                 type: 'string',
                 demand: true
             }).argv;
+        this.funcToRun = this.getFuncToRun();
+        this.input = this.argv.input;
     }
 
-    convertString(input: string, func: string): string {
+    convertString(input: string, func: StringFunction): string {
         let output: string;
 
-        switch(func) {
-            case StringFunction.CAMEL_CASE: {
-                output = voca.camelCase(input);
-                break;
-            }
-            case StringFunction.KEBAB_CASE: {
-                output = voca.kebabCase(input);
-                break;
-            }
-            default: {
-                throw new Error(`Can't find function ${this.argv.function}`);
-            }
+        if (voca[func.methodName]) {
+            output = voca[func.methodName](input);
+        } else {
+            throw new Error(`Can't find function ${this.argv.function}`);
         }
         return output;
     }
@@ -51,12 +50,18 @@ class Casey {
     generateDescription(): string {
         let desc: string[] = [];
         desc.push('choose a function to run');
-        Object.entries(StringFunction).forEach(func => {
-            const [key, value] = func;
-            desc.push(`${value} => ${this.convertString(key, value)}`);
+        this.availableFunc.forEach(func => {
+            desc.push(`${func.alias} => ${this.convertString(func.name, func)}`);
         });
-        desc.push('');
         return desc.join('\n')
+    }
+
+    getFuncToRun(): StringFunction {
+        let funcToRun = this.availableFunc.find(func => func.alias === this.argv.function);
+        if (!funcToRun) {
+            throw new Error(`Can't find function ${this.argv.function}`);
+        }
+        return funcToRun
     }
 }
 
--- src/string-function.ts
+++ src/string-function.ts
@@ -1,4 +1,45 @@
-export enum StringFunction {
-    CAMEL_CASE = 'cc',
-    KEBAB_CASE = 'kc'
+export interface StringFunction {
+    name: string,
+    alias: string,
+    methodName: string
+}
+
+export function getAvailableFunctions(): StringFunction[] {
+    return [{
+        name: 'To Camel Case',
+        alias: 'cc',
+        methodName: 'camelCase'
+    },{
+        name: 'To Capitalised',
+        alias: 'c',
+        methodName: 'capitalize'
+    },{
+        name: 'To Decapitalised',
+        alias: 'd',
+        methodName: 'decapitalize'
+    },{
+        name: 'To Kebab Case',
+        alias: 'kc',
+        methodName: 'kebabCase'
+    },{
+        name: 'To Lower Case',
+        alias: 'lc',
+        methodName: 'lowerCase'
+    },{
+        name: 'To Snake Case',
+        alias: 'sc',
+        methodName: 'snakeCase'
+    },{
+        name: 'To Swap Case',
+        alias: 'swc',
+        methodName: 'swapCase'
+    },{
+        name: 'To Title Case',
+        alias: 'tc',
+        methodName: 'titleCase'
+    },{
+        name: 'To Upper Case',
+        alias: 'uc',
+        methodName: 'upperCase'
+    }]
 }
