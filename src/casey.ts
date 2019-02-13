ef6cca36b195ba047653beac9990c52ddf4727a1
Matthew Alner <matthew.alner@senseye.io>
2019-02-13 13:19:32 +0000

    add run from bin

--- src/casey.ts
+++ src/casey.ts
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
+import * as yargs from 'yargs'
 
+const argv = yargs.option('size', {
+    alias: 's',
+    describe: 'choose a size',
+    choices: ['xs', 's', 'm', 'l', 'xl']
+}).argv;
 
 console.log("hello");
+console.log("size: ", argv.size);
