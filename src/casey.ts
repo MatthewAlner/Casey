29f1970f4bdff6db870c589524adf28a6bcfccde
Matthew Alner <matthew.alner@senseye.io>
2019-02-13 13:55:43 +0000

    working

--- src/casey.ts
+++ src/casey.ts
@@ -1,11 +1,36 @@
 #!/usr/bin/env node
 import * as yargs from 'yargs'
+import * as voca from 'voca';
 
-const argv = yargs.option('size', {
-    alias: 's',
-    describe: 'choose a size',
-    choices: ['xs', 's', 'm', 'l', 'xl']
+import { StringFunction } from './string-function';
+
+const desc = `
+choose a function to run
+    cc => toCamelCase
+    kc => to-kebab-case
+`;
+
+const argv = yargs
+    .option('function', {
+    alias: 'f',
+    describe: desc,
+    type: 'string',
+    demand: true,
+    choices: Object.keys(StringFunction).map((k) => StringFunction[k])
+}).option('input', {
+    alias: 'i',
+    describe: 'provide string to transform',
+    type: 'string',
+    demand: true
 }).argv;
 
-console.log("hello");
-console.log("size: ", argv.size);
+switch(argv.function) {
+    case StringFunction.CAMEL_CASE: {
+        console.log(voca.camelCase(argv.input));
+        break;
+    }
+    case StringFunction.KEBAB_CASE: {
+        console.log(voca.kebabCase(argv.input));
+        break;
+    }
+}
---
+++ src/string-function.ts
@@ -0,0 +1,4 @@
+export enum StringFunction {
+    CAMEL_CASE = 'cc',
+    KEBAB_CASE = 'kc'
+}
